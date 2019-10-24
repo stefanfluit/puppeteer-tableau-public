09236ed3ea2eb15501502ec8bbfcde63fa5f7bcb
Stefan Fluit <fluit@datlinq.com>
2019-10-24 14:41:26 +0200

    Added .sh

--- script.sh
+++ script.sh
@@ -8,6 +8,8 @@ set -o errexit      	# Exit on uncaught errors
 set -o nounset      	# Dont allow unset variables
 set -o pipefail     	# Fail pipe on first error
 
+declare git_location="/var/puppeteer-tableau"
+
 # Functions
 check_sudo() {
 	if [ `/usr/bin/whoami` != 'root' ];then
@@ -19,12 +21,13 @@ check_sudo() {
 fetch_tableau_server() {
     local distro="${1}"
     local final_url=$(docker logs puppeteer-tableau | grep downloads)
-    declare docker_volume="/var/puppeteer-tableau/src:/src"
+    local docker_volume="/var/puppeteer-tableau/src:/src"
     docker run --name "puppeteer-tableau" --network="puppeteer-tableau_default" --volume="${docker_volume}" --ipc="shareable" -d nodejs:image npm run start -- 64."${distro}" && sleep 8
     wget "${final_url}" && docker rm -f puppeteer-tableau
 }
 
 main() {
+    cd "${git_location}"; docker-compose up;
     check_sudo && fetch_tableau_server "${1}"
 }
 
