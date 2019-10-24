a2b46075bb443a5761295dcf85ca414acc0c3d12
Stefan Fluit <fluit@datlinq.com>
2019-10-24 14:39:13 +0200

    Added .sh

--- script.sh
+++ script.sh
@@ -8,10 +8,6 @@ set -o errexit      	# Exit on uncaught errors
 set -o nounset      	# Dont allow unset variables
 set -o pipefail     	# Fail pipe on first error
 
-# Variables
-declare git_location=$(ls /sys/class/net)
-declare docker_volume="/var/puppeteer-tableau/src:/src"
-
 # Functions
 check_sudo() {
 	if [ `/usr/bin/whoami` != 'root' ];then
@@ -23,9 +19,13 @@ check_sudo() {
 fetch_tableau_server() {
     local distro="${1}"
     local final_url=$(docker logs puppeteer-tableau | grep downloads)
-    docker run --name puppeteer-tableau --network="puppeteer-tableau_default" --volume="/var/puppeteer-tableau/src:/src" --ipc="shareable" -d nodejs:image npm run start -- 64.${distro} && sleep 8
+    declare docker_volume="/var/puppeteer-tableau/src:/src"
+    docker run --name "puppeteer-tableau" --network="puppeteer-tableau_default" --volume="${docker_volume}" --ipc="shareable" -d nodejs:image npm run start -- 64."${distro}" && sleep 8
     wget "${final_url}" && docker rm -f puppeteer-tableau
+}
 
+main() {
+    check_sudo && fetch_tableau_server "${1}"
+}
 
-sleep 8 && wget $(docker logs puppeteer-tableau | grep downloads)
-#docker rm -f puppeteer-tableau
+main
