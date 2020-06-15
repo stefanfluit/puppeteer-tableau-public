05ecb0e05c19df7f39782add18b3527d2a03d675
stefanfluit <41238956+stefanfluit@users.noreply.github.com>
2020-06-15 11:51:15 +0200

    Update fetch_tableau.sh

--- fetch_tableau.sh
+++ fetch_tableau.sh
@@ -7,18 +7,10 @@
 set -o errexit      	# Exit on uncaught errors
 set -o pipefail     	# Fail pipe on first error
 
-declare git_location="/var/puppeteer-tableau"
+declare git_location="/var/puppeteer-tableau-public"
 declare distro="${1}"
 
 # Functions
-check_args_container() {
-    if [[ "${distro}" -eq 0 ]]
-    then
-      printf "No arguments supplied.\nSyntax is like:\n sudo ./fetch_tableau.sh deb/rpm \n"
-      exit 1;
-    fi
-}
-
 check_sudo() {
 	if [ `/usr/bin/whoami` != 'root' ];then
 		echo 'Error: run script with sudo'
@@ -27,14 +19,13 @@ check_sudo() {
 }
 
 fetch_tableau_server() {
-    local docker_volume="/var/puppeteer-tableau/src:/src"
+    local docker_volume="/var/puppeteer-tableau-public/src:/src"
     docker run --name "puppeteer-tableau" --network="puppeteer-tableau_default" --volume="${docker_volume}" --ipc="shareable" -d nodejs:image npm run start -- 64."${distro}" && sleep 10
     echo $(docker logs puppeteer-tableau | grep downloads)
     docker rm -f puppeteer-tableau
 }
 
 main() {
-    check_args
     # You can uncomment this compose up command if this is not the first time running it. 
     cd "${git_location}"; docker-compose up;
     check_sudo && fetch_tableau_server "${distro}"
